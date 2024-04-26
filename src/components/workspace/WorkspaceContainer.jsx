093d236bc48440b3f080e5cc0ad42e07a33baefc
Nithishpandian <nithishpandiyan75@gmail.com>
2024-04-27 00:06:41 +0530

    added workspace

--- src/components/workspace/WorkspaceContainer.jsx
+++ src/components/workspace/WorkspaceContainer.jsx
@@ -1,9 +1,57 @@
-import React from 'react'
+import React from "react";
+import { CiSearch } from "react-icons/ci";
+import workspaceImg from "../../assets/workspace/workspace-img.png";
 
 const WorkspaceContainer = () => {
   return (
-    <div>WorkspaceContainer</div>
-  )
-}
+    <div className=" flex flex-col gap-4 p-4 w-full">
+      <div className=" flex items-center justify-between w-full">
+        <h1 className=" text-stone-800 font-bold text-2xl">Workspaces</h1>
+        <button className=" text-white font-medium bg-sky-800 text-sm px-4 py-1 rounded">
+          Create workspace
+        </button>
+      </div>
+      <div className=" flex">
+        <input
+          className=" border border-stone-300 py-1 px-2 rounded-l-md text-sm placeholder:text-stone-400"
+          placeholder="Filter workspaces"
+          type="text"
+        />
+        <div className=" border border-stone-300 pt-1.5 px-2 rounded-r-md">
+          <CiSearch />
+        </div>
+      </div>
+      <table className="">
+        <tr className=" text-stone-700 text-base border-b border-stone-300">
+          <th>Name</th>
+          <th>Status</th>
+          <th>Pricing tier</th>
+          <th>Region</th>
+          <th>Bucket name</th>
+          <th>Credential name</th>
+          <th>Created</th>
+          <th>Metastore</th>
+        </tr>
+      </table>
+      <div className=" bg-sky-50 py-4 px-4 my-5 flex items-center justify-center w-full">
+        <div className=" bg-white px-5 pb-4 w-96 flex flex-col gap-3">
+          <div className=" flex flex-col items-center">
+            <img src={workspaceImg} alt="" />
+            <h1 className=" text-stone-800 font-semibold">Workspaces</h1>
+          </div>
+          <p className=" text-sm text-stone-600 font-medium text-center">
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+            Accusantium, fuga.
+          </p>
+          <div className=" flex items-center justify-center">
+            <button className=" text-white font-medium bg-sky-800 text-sm px-4 py-1 rounded">
+              Create workspace
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default WorkspaceContainer
+export default WorkspaceContainer;
